117be4fa8d7156716ec205ad68ae128d6cc7e8e8
Navindu <navindushane@gmail.com>
2018-08-20 21:21:25 +0530

    invenotry JS file

--- js/inventoryJS.js
+++ js/inventoryJS.js
@@ -1,84 +1,29 @@
-$(document).ready(function() {
-    $("#add_row").on("click", function() {
-        // Dynamic Rows Code
 
-        // Get max row id and set new id
-        var newid = 0;
-        $.each($("#tab_logic tr"), function() {
-            if (parseInt($(this).data("id")) > newid) {
-                newid = parseInt($(this).data("id"));
-            }
-        });
-        newid++;
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
 
-        var tr = $("<tr></tr>", {
-            id: "addr"+newid,
-            "data-id": newid
-        });
+    trigger.click(function () {
+      hamburger_cross();
+    });
 
-        // loop through each td and create new elements with name of newid
-        $.each($("#tab_logic tbody tr:nth(0) td"), function() {
-            var cur_td = $(this);
+    function hamburger_cross() {
 
-            var children = cur_td.children();
+      if (isClosed == true) {
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
 
-            // add new td and element if it has a nane
-            if ($(this).data("name") != undefined) {
-                var td = $("<td></td>", {
-                    "data-name": $(cur_td).data("name")
-                });
-
-                var c = $(cur_td).find($(children[0]).prop('tagName')).clone().val("");
-                c.attr("name", $(cur_td).data("name") + newid);
-                c.appendTo($(td));
-                td.appendTo($(tr));
-            } else {
-                var td = $("<td></td>", {
-                    'text': $('#tab_logic tr').length
-                }).appendTo($(tr));
-            }
-        });
-
-        // add delete button and td
-        /*
-        $("<td></td>").append(
-            $("<button class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>")
-                .click(function() {
-                    $(this).closest("tr").remove();
-                })
-        ).appendTo($(tr));
-        */
-
-        // add the new row
-        $(tr).appendTo($('#tab_logic'));
-
-        $(tr).find("td button.row-remove").on("click", function() {
-             $(this).closest("tr").remove();
-        });
-});
-
-
-
-
-    // Sortable Code
-    var fixHelperModified = function(e, tr) {
-        var $originals = tr.children();
-        var $helper = tr.clone();
-
-        $helper.children().each(function(index) {
-            $(this).width($originals.eq(index).width())
-        });
-
-        return $helper;
-    };
-
-    $(".table-sortable tbody").sortable({
-        helper: fixHelperModified
-    }).disableSelection();
-
-    $(".table-sortable thead").disableSelection();
-
-
-
-    $("#add_row").trigger("click");
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });
 });
