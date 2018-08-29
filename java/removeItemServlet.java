eb8e4ad5bd37a47a9a44ab2a718472a6df0a7138
Navindu <navindushane@gmail.com>
2018-08-29 13:54:24 +0530

    remove item servlet added

---
+++ java/removeItemServlet.java
@@ -0,0 +1,79 @@
+package backend;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class removeItemServlet
+ */
+@WebServlet("/removeItemServlet")
+public class removeItemServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public removeItemServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+
+		response.setContentType("text/html");
+
+		String item = request.getParameter("itemCode");
+		int product = Integer.parseInt(item);
+
+
+		//setting up DB connection
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sujanee_hotel", "root", "root");
+
+			Statement st = connection.createStatement();
+
+			st.executeUpdate("delete from menu_mgmt where menu_id = ('"+item+"')");
+
+			System.out.println("Deletion successful!");
+
+
+		}catch(Exception e) {
+
+			System.out.print(e);
+			e.printStackTrace();
+
+		}
+		//Re-directs to the following page
+		RequestDispatcher dispatch = request.getRequestDispatcher("menuManagement.jsp");
+        dispatch.forward(request, response);
+
+
+
+	}
+
+}
