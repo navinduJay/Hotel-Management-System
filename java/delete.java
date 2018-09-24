3161575639502a7bc5434252c03a46ad250d47d1
DinuwanB <dinubhagya97@gmail.com>
2018-09-24 22:48:16 +0530

    Room anf Hall delete servlet add

---
+++ java/delete.java
@@ -0,0 +1,59 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class delete
+ */
+@WebServlet("/delete")
+public class delete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public delete() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String tid = request.getParameter("tid");
+		System.out.print(tid);
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root","root");
+			Statement st=conn.createStatement(); 
+			st.executeUpdate("DELETE FROM mhr WHERE id='"+tid+"'");
+			System.out.println("Data Deleted Successfully!"); } 
+
+
+		catch(Exception e)
+			{ System.out.print(e); e.printStackTrace(); }
+	}
+
+}
---
+++ java/room_delete.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class room_delete
+ */
+@WebServlet("/room_delete")
+public class room_delete extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public room_delete() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		String rid = request.getParameter("rid");
+		System.out.print(rid);
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db1", "root","root");
+			Statement st=conn.createStatement(); 
+			st.executeUpdate("DELETE FROM mrr WHERE cid='"+rid+"'");
+			System.out.println("Data Deleted Successfully!"); } 
+
+
+		catch(Exception e)
+			{ System.out.print(e); e.printStackTrace(); }
+	}
+		
+	}
+
+
